3a740fdbe09162557e80b7a0090da477e36b2f66
Hasan Hakan ERDEN <81776038+Hakanerdn@users.noreply.github.com>
2024-03-18 18:04:25 +0300

    Product CRUD Part 1

--- GHWeb.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ GHWeb.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace GHWeb.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
         void Save();
     }
 }
--- GHWeb.DataAccess/Repository/UnitOfWork.cs
+++ GHWeb.DataAccess/Repository/UnitOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using GHWeb.DataAccess.Data;
 using GHWeb.DataAccess.Repository.IRepository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GHWeb.DataAccess.Repository
 {
@@ -12,10 +7,14 @@ namespace GHWeb.DataAccess.Repository
     {
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
+
+        public IProductRepository Product { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }
 
         public void Save()
