5f87f3ce6681a66d56443c054cecbe120ba8afd3
Hasan Hakan ERDEN <81776038+Hakanerdn@users.noreply.github.com>
2024-03-31 18:09:18 +0300

    Order Confirmation

--- GHWeb.DataAccess/Migrations/20240331113201_addOrderHeaderAndDetail.Designer.cs
+++ GHWeb.DataAccess/Migrations/20240331113201_addOrderHeaderAndDetail.Designer.cs
@@ -4,6 +4,7 @@ using GHWeb.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GHWeb.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240331113201_addOrderHeaderAndDetail")]
+    partial class addOrderHeaderAndDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
